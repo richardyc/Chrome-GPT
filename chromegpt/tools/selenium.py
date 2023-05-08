74882735a1ff277d55c1ad3b1e9b05c2341256dc
ethanthai2 <44010708+ethanthai2@users.noreply.github.com>
2023-05-08 14:59:41 +0700

    Update selenium.py

--- chromegpt/tools/selenium.py
+++ chromegpt/tools/selenium.py
@@ -15,6 +15,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
 
 from webdriver_manager.chrome import ChromeDriverManager
 
