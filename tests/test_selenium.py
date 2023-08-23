6294b54fd38f1515174b3aef71002d6a103146da
Erlich Sefi <erlichsefi@gmail.com>
2023-08-23 20:42:36 +0300

    Update test_selenium.py

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -1,47 +1,68 @@
 """Integration test for Selenium API Wrapper."""
 
-import pytest
+# import pytest
 
 from chromegpt.tools.selenium import SeleniumWrapper
 
 
-@pytest.fixture
-def client() -> SeleniumWrapper:
-    return SeleniumWrapper(headless=True)
+# @pytest.fixture
+# def client() -> SeleniumWrapper:
+#     return SeleniumWrapper(headless=True)
 
 
-def test_describe_website(client: SeleniumWrapper) -> None:
+def test_describe_website() -> None:
     """Test that SeleniumWrapper returns correct website"""
+    client = SeleniumWrapper(headless=True)
+    output = None
+    try:
+        output = client.describe_website("https://example.com")
+    finally:
+        del client
+    assert output is not None and "this domain is for use in illu" in output
 
-    output = client.describe_website("https://example.com")
-    assert "this domain is for use in illu" in output
 
-
-def test_click(client: SeleniumWrapper) -> None:
+def test_click() -> None:
     """Test that SeleniumWrapper click works"""
+    client = SeleniumWrapper(headless=True)
+    output = None
+    try:
+        client.describe_website("https://example.com")
+        output = client.click_button_by_text('link with title "More information..."')
+    finally:
+        del client
+    assert output is not None and "Clicked interactable element and the website changed" in output
 
-    client.describe_website("https://example.com")
-    output = client.click_button_by_text('link with title "More information..."')
-    assert "Clicked interactable element and the website changed" in output
 
-
-def test_google_input(client: SeleniumWrapper) -> None:
+def test_google_input() -> None:
     """Test that SeleniumWrapper can find input form"""
+    client = SeleniumWrapper(headless=True)
+    output = None
+    try:
+        output = client.find_form_inputs("https://google.com")
+    finally:
+        del client
+    assert output is not None and "q" in output
 
-    output = client.find_form_inputs("https://google.com")
-    assert "q" in output
 
-
-def test_google_fill(client: SeleniumWrapper) -> None:
+def test_google_fill() -> None:
     """Test that SeleniumWrapper can fill input form"""
+    client = SeleniumWrapper(headless=True)
+    output = None
+    try:
+        client.find_form_inputs("https://google.com")
+        output = client.fill_out_form(q="hello world")
+    finally:
+        del client
+    assert output is not None and "website changed after filling out form" in output
 
-    client.find_form_inputs("https://google.com")
-    output = client.fill_out_form(q="hello world")
-    assert "website changed after filling out form" in output
 
-
-def test_google_search(client: SeleniumWrapper) -> None:
+def test_google_search() -> None:
     """Test google search functionality"""
-    res = client.google_search("hello world")
-    assert "hello" in res
-    assert "Which url would you like to goto" in res
+    client = SeleniumWrapper(headless=True)
+    output = None
+    try:
+        output = client.google_search("hello world")
+    finally:
+        del client
+    assert output is not None and "hello" in output
+    assert output is not None and "Which url would you like to goto" in output
