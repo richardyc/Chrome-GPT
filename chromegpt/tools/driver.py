894223dcfe429ae8b27b41c2ce8cd45b35f77752
Erlich Sefi <erlichsefi@gmail.com>
2023-08-30 11:31:23 +0000

    .

--- chromegpt/tools/driver.py
+++ chromegpt/tools/driver.py
@@ -1,12 +1,14 @@
 from chromegpt.tools.selenium import SeleniumWrapper
 
+
 def execute_with_driver(test_function):
     def wrapper(*args, **kwargs):
         try:
             client = SeleniumWrapper(headless=True)
             result = test_function(*args, **kwargs, client=client)
         finally:
-            # release the driver  
+            # release the driver
             del client
         return result
+
     return wrapper
--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -1,19 +1,20 @@
 """Integration test for Selenium API Wrapper."""
 
-from chromegpt.tools.driver import execute_with_driver,SeleniumWrapper
+from chromegpt.tools.driver import execute_with_driver, SeleniumWrapper
 
 
 @execute_with_driver
-def test_describe_website(client:SeleniumWrapper) -> None:
+def test_describe_website(client: SeleniumWrapper) -> None:
     """Test that SeleniumWrapper returns correct website"""
     output = None
     output = client.describe_website("https://example.com")
     assert output is not None and "this domain is for use in illu" in output
 
+
 @execute_with_driver
-def test_click(client:SeleniumWrapper) -> None:
+def test_click(client: SeleniumWrapper) -> None:
     """Test that SeleniumWrapper click works"""
-   
+
     output = None
     client.describe_website("https://example.com")
     output = client.click_button_by_text('link with title "More information..."')
@@ -22,15 +23,17 @@ def test_click(client:SeleniumWrapper) -> None:
         and "Clicked interactable element and the website changed" in output
     )
 
+
 @execute_with_driver
-def test_google_input(client:SeleniumWrapper) -> None:
+def test_google_input(client: SeleniumWrapper) -> None:
     """Test that SeleniumWrapper can find input form"""
     output = None
     output = client.find_form_inputs("https://google.com")
     assert output is not None and "q" in output
 
+
 @execute_with_driver
-def test_google_fill(client:SeleniumWrapper) -> None:
+def test_google_fill(client: SeleniumWrapper) -> None:
     """Test that SeleniumWrapper can fill input form"""
     output = None
     client.find_form_inputs("https://google.com")
@@ -38,8 +41,9 @@ def test_google_fill(client:SeleniumWrapper) -> None:
 
     assert output is not None and "website changed after filling out form" in output
 
+
 @execute_with_driver
-def test_google_search(client:SeleniumWrapper) -> None:
+def test_google_search(client: SeleniumWrapper) -> None:
     """Test google search functionality"""
     output = None
     output = client.google_search("hello world")
